cffe07df3152f375a7b1a0a93dc43b13375e409d
kumarsarak <saravana.k@live.com>
2014-07-15 10:15:52 -0700

    Authorization to Download and fix the 404 error in download, add the deployment document

--- WebRole1/downloadDocument.aspx.cs
+++ WebRole1/downloadDocument.aspx.cs
@@ -20,31 +20,62 @@ namespace WebRole1
 
             if (Request.QueryString["recordNumber"] != null && Request.QueryString["docType"] != null)
             {
-                recNum = (Request.QueryString["recordNumber"]).Trim();
-                containerNameConStr = (Request.QueryString["docType"]).Trim() + containerNameConStr;
-                storageConStr = (Request.QueryString["docType"]).Trim() + storageConStr;
+                if (authorizedownload())
+                {
+                    try
+                    {
+                        recNum = (Request.QueryString["recordNumber"]).Trim();
+                        containerNameConStr = (Request.QueryString["docType"]).Trim() + containerNameConStr;
+                        storageConStr = (Request.QueryString["docType"]).Trim() + storageConStr;
 
-                String containerName = ConfigurationManager.ConnectionStrings[containerNameConStr].ConnectionString;
+                        String containerName = ConfigurationManager.ConnectionStrings[containerNameConStr].ConnectionString;
 
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                            ConfigurationManager.ConnectionStrings[storageConStr].ConnectionString);
+                        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                                    ConfigurationManager.ConnectionStrings[storageConStr].ConnectionString);
 
-                // Create the blob client.
-                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+                        // Create the blob client.
+                        CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                        CloudBlobContainer container = blobClient.GetContainerReference(containerName);
 
-                // Retrieve reference to a blob named blobName
-                CloudBlockBlob blockBlob = container.GetBlockBlobReference(recNum);
+                        // Retrieve reference to a blob named blobName
+                        CloudBlockBlob blockBlob = container.GetBlockBlobReference(recNum);
 
-                System.IO.MemoryStream mem = new System.IO.MemoryStream();
-                Response.AddHeader("Content-Disposition", "attachment; filename=" + recNum + ".tif");
-                blockBlob.DownloadToStream(mem);
-                Response.BinaryWrite(mem.ToArray());
+                        System.IO.MemoryStream mem = new System.IO.MemoryStream();
+                        Response.AddHeader("Content-Disposition", "attachment; filename=" + recNum + ".tif");
+                        blockBlob.DownloadToStream(mem);
+                        Response.BinaryWrite(mem.ToArray());
+                    }
+
+                    catch (Exception ex)
+                    {
+                        if (ex.Message.Contains("404"))
+                        {
+                            Response.Write("No Document found");
+                        }
+                    }
+                }
             }
             else
             {
-                Response.Write("Error!!  Please add more error handling logic here .....");
+                Response.Write("Please provide the parameters");
             }
+        }
+
+        private bool authorizedownload()
+        {
+            if (HttpContext.Current.Session["Role"] != null  && 
+                ((HttpContext.Current.Session["Role"].ToString().ToLower() == "dci") ||
+                 (HttpContext.Current.Session["Role"].ToString().ToLower() == "apadmin") ||
+                 (HttpContext.Current.Session["Role"].ToString().ToLower() == "aradmin") ||
+                 (HttpContext.Current.Session["Role"].ToString().ToLower() == "apuser") ||
+                 (HttpContext.Current.Session["Role"].ToString().ToLower() == "aruser") 
+                )
+               )
+
+                return true;
+            else
+                return false;
+            
         }
     }
 }
